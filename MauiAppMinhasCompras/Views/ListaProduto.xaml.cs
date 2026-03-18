27336db83c1b4a1f560d1a5d8f5699a9f14d2e46
Lincoln Oliveira <lincoln_196@hotmail.com>
2026-03-18 03:08:46 -0300

    Desenvolvimento Realizado na Agenda 05 de DS-III

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -5,86 +5,142 @@ namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 
-	public ListaProduto()
-	{
-		InitializeComponent();
+    public ListaProduto()
+    {
+        InitializeComponent();
 
-		lst_produtos.ItemsSource = lista;
-	}
+        lst_produtos.ItemsSource = lista;
+    }
 
     protected async override void OnAppearing()
     {
-		List<Produto> tmp = await App.Db.GetAll();
+        try
+        {
+            List<Produto> tmp = await App.Db.GetAll();
 
-		tmp.ForEach(i => lista.Add(i));
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-			Navigation.PushAsync(new Views.NovoProduto());
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
 
-		}catch (Exception ex)
-		{
-			DisplayAlert("Ops", ex.Message, "OK");
-		}
-	}
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
+        try
+        {
+            string q = e.NewTextValue;
 
-		lista.Clear();
+            lista.Clear();
 
-        List<Produto> tmp = await App.Db.Search(q);
+            List<Produto> tmp = await App.Db.Search(q);
 
-        tmp.ForEach(i => lista.Add(i));
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
-		double soma = lista.Sum(i => i.Total);
+        double soma = lista.Sum(i => i.Total);
 
-		string msg = $"O total é {soma:C}";
+        string msg = $"O total é {soma:C}";
 
-		DisplayAlert("Total dos Produtos", msg, "OK");
+        DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
-    // Remover pelo MenuItem (ação de contexto)
     private async void MenuItem_Clicked(object sender, EventArgs e)
     {
-        var mi = (MenuItem)sender;
-        var produto = (Produto)mi.BindingContext;
+        try
+        {
+            MenuItem selecionado = sender as MenuItem;
 
-        bool confirm = await DisplayAlert("Remover", $"Deseja remover {produto.Descricao}?", "Sim", "Não");
+            Produto p = selecionado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert(
+                "Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto;
+
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p,
+            });
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+}
+/*
+// Remover pelo MenuItem (ação de contexto)
+private async void MenuItem_Clicked(object sender, EventArgs e)
+{
+
+    var mi = (MenuItem)sender;
+    var produto = (Produto)mi.BindingContext;
+
+    bool confirm = await DisplayAlert("Remover", $"Deseja remover {produto.Descricao}?", "Sim", "Não");
+    if (confirm)
+    {
+        await App.Db.Delete(produto.Id);
+        lista.Remove(produto);
+    }
+}
+
+// Remover pelo botão da Toolbar (item selecionado)
+private async void ToolbarItem_Clicked_Remover(object sender, EventArgs e)
+{
+    if (lst_produtos.SelectedItem is Produto produto)
+    {
+        bool confirm = await DisplayAlert("Remover",
+            $"Deseja remover o produto \"{produto.Descricao}\"?",
+            "Sim", "Não");
+
         if (confirm)
         {
             await App.Db.Delete(produto.Id);
             lista.Remove(produto);
         }
     }
-
-    // Remover pelo botão da Toolbar (item selecionado)
-    private async void ToolbarItem_Clicked_Remover(object sender, EventArgs e)
+    else
     {
-        if (lst_produtos.SelectedItem is Produto produto)
-        {
-            bool confirm = await DisplayAlert("Remover",
-                $"Deseja remover o produto \"{produto.Descricao}\"?",
-                "Sim", "Não");
-
-            if (confirm)
-            {
-                await App.Db.Delete(produto.Id);
-                lista.Remove(produto);
-            }
-        }
-        else
-        {
-            await DisplayAlert("Aviso", "Selecione um produto na lista para remover.", "OK");
-        }
+        await DisplayAlert("Aviso", "Selecione um produto na lista para remover.", "OK");
     }
-}
+    */
